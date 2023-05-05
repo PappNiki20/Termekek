51c9159da51b5817aa9cb167953ddb8c9b0ecbf2
PappNiki20 <papp.nikoletta001@gmail.com>
2023-05-05 10:21:39 +0200

    jsonnel is mukodik

---
+++ Asszinkron.js
@@ -0,0 +1,20 @@
+class Asszinkron {
+    
+  constructor() {}
+  
+  adatBeolvas(vegpont, callbackFv) {
+    fetch(vegpont, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+   
+      .then((response) => response.json())
+      .then((data) => {
+        callbackFv(data);
+      })
+      .catch((error) => console.log(error));
+  }
+}
+export default Asszinkron;
--- Termekek.js
+++ Termekek.js
@@ -1,20 +1,31 @@
 import { adat } from "./adatok.js";
 import Termek from "/Termek.js";
+import Asszinkron from "./Asszinkron.js";
 class Termekek {
   #kedvencek = [];
   constructor() {
-    const SZULOELEM = $("article");
+    /*adat beolv */
+    const asszinkron = new Asszinkron();
+    let vegpont = "adat.json";
+    asszinkron.adatBeolvas(vegpont, this.#termekmegjelenito);
 
-    for (let index = 0; index < adat.length; index++) {
-      const Termek1 = new Termek(adat[index], SZULOELEM);
-    }
     /* a function csak a html elemre vonatkozik*/
     $(window).on("gombkattintas", (event) => {
-      this.#kedvencek.push(event.detail)
+      this.#kedvencek.push(event.detail);
       console.log(this.#kedvencek);
       console.log(event.detail);
     });
     console.log(this.#kedvencek);
+  }
+  #termekmegjelenito(lista) {
+    let sajatlista = lista.adat
+    /* megjeleníti az obj.*/
+    /* a listát a json fájlból kajuk*/
+    const SZULOELEM = $("article");
+    console.log(sajatlista)
+    for (let index = 0; index < sajatlista.length; index++) {
+      const Termek1 = new Termek(sajatlista[index], SZULOELEM, index);
+    }
   }
 }
 export default Termekek;
